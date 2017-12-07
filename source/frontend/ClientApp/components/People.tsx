dbd03caca923161776a28d80cac631074af4bc14
Terje Nagel <terje.nagel@gmail.com>
2017-12-07 23:36:05 +0100

    Working on 'People'

--- source/frontend/ClientApp/components/People.tsx
+++ source/frontend/ClientApp/components/People.tsx
@@ -2,6 +2,12 @@ import * as React from 'react';
 import { RouteComponentProps } from 'react-router';
 import 'isomorphic-fetch';
 
-export class People extends React.Component {
+export class People extends React.Component<RouteComponentProps<{}>> {
     
+    public render() {
+        return <div>
+            Hello World
+        </div>;
+    }
+
 }
